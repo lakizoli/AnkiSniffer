f8b58b017e9df7799529ccdb6c5e58e9f5623bb4
Zoltan Laki <lakizoli@yahoo.com>
2016-05-19 11:16:07 +0200

    Folyt kov otthon...

--- AnkiSniffer/MainWindow.xaml.cs
+++ AnkiSniffer/MainWindow.xaml.cs
@@ -18,8 +18,29 @@ namespace AnkiSniffer {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        #region PackagePath property
+        public string PackagePath
+        {
+            get { return (string)GetValue (PackagePathProperty); }
+            set { SetValue (PackagePathProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PackagePath.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PackagePathProperty =
+            DependencyProperty.Register ("PackagePath", typeof (string), typeof (MainWindow), new PropertyMetadata (null));
+        #endregion
+
         public MainWindow () {
             InitializeComponent ();
+
+            this.DataContext = this;
+
+            PackagePath = @"d:\work\Erwin_Tschirner_Angol_szkincs__Best_of_English_-_en-hu.apkg";
+
+            Reload ();
+        }
+
+        private void Reload () {
         }
     }
 }
